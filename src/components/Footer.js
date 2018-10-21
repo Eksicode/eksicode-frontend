703e6e56abdd3ad05587eb20098e01648b3752ff
AtakanErmis <ataerms@gmail.com>
2018-10-21 14:40:18 +0300

    Footer düzenlendi

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ class Footer extends Component {
       <div className="footer center">
         <div className="container">
           <div class="row center">
-            <a href="https://github.com/Eksicode" target="_blank"><i class="logos-footer devicon-github-plain colored"></i></a>
-            <a href="https://twitter.com/eksicode" target="_blank"><i class="logos-footer devicon-twitter-plain colored"></i></a><br></br>
-            2018 eksicode.org
+            <a href="https://github.com/Eksicode" target="_blank"><i class="logos-footer devicon-github-plain"></i></a>
+            <a href="https://twitter.com/eksicode" target="_blank"><i class="logos-footer devicon-twitter-plain"></i></a>
+            <p>2018 - eksicode.org</p>
           </div>
         </div>
       </div>
