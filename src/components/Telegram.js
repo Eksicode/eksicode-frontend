8fcf06069681a712567b9e0975d040fe1f7fb532
AtakanErmis <ataerms@gmail.com>
2019-07-20 23:15:06 +0300

    sıkıldım

--- src/components/Telegram.js
+++ src/components/Telegram.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import TelegramLogo from "../assets/telegram.svg";
 
-const Telegram = () => {
-  document.title = "Eksicode.org - Telegram Grupları";
-  return (
-    <div className="container">
+class Telegram extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groups: []
+    }
+  }
+  
+  componentDidMount() {
+    document.title = "Eksicode.org - Telegram Grupları";
+    fetch("http://api.eksicode.org/telegrams")
+    .then(res => res.json())
+    .then(list => this.setState({groups: list}))
+  }
+  componentDidUpdate() {
+    console.log(this.state)
+  }
+  render() {
+    return (
+      <div className="container">
       <div className="row">
         <div className="col m12 l6">
           <h2>Telegram Grupları</h2>
@@ -18,338 +35,38 @@ const Telegram = () => {
             target="_blank"
             className="btn-small blue waves-effect waves"
           >
-            <img src={require("../assets/telegram.svg")} height="12em" />
-            Telegramı İndir
+            <img src={TelegramLogo} height="12em" />
+            Telegram'ı İndir
           </a>
         </div>
       </div>
+      
       <div className="row center" id="channels">
-        <div className="col s12 m4 l3">
+        {this.state.groups ? this.state.groups.map(e => {
+        return (
+          <div className="col s12 m4 l3">
           <div className="card hoverable">
             <div className="card-content black-text">
-              <i className="logos material-icons">code</i>
-              <h4>Genel Konular</h4>
-              <p>121 üye</p>
+              <i className={`logos ${e.logo}`}>{e.icon}</i>
+              <h4>{e.name}</h4>
+              <p>{e.members} Üye</p>
             </div>
             <a
-              href="https://t.me/joinchat/G0kqtVFVFl2cWMnmJdUniw"
+              href={e.link}
               target="_blank"
               className="btn-small waves-effect waves"
             >
-              <img src={require("../assets/telegram.svg")} height="12em" />
+              <img className="telegram__logo" src={TelegramLogo} alt="" height="12em" />
               Katıl
             </a>
           </div>
         </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos devicon-javascript-plain colored" />
-              <h4>JavaScript</h4>
-              <p>189 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtQ2bMHbsHgiIMKMpyg"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className=" logos devicon-python-plain colored" />
-              <h4>Python</h4>
-              <p>224 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtQ-rkgcsqiPbm4q_6g"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos devicon-php-plain colored" />
-              <h4>php</h4>
-              <p>45 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtUgwV5JmLHug4VBwpA"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos devicon-cplusplus-plain colored" />
-              <h4>C++</h4>
-              <p>36 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtUgjXMM2jDEysQFTsA"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos devicon-c-plain colored" />
-              <h4>C</h4>
-              <p>36 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtQ0-rHk9g1v61dUWsA"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos devicon-csharp-plain colored" />
-              <h4>C#</h4>
-              <p>41 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtU9zTmZKbyCpNqkobA"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos devicon-java-plain colored" />
-              <h4>Java</h4>
-              <p>36 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtVLK6GPjF1BrtzJQlg"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos devicon-ruby-plain colored" />
-              <h4>Ruby</h4>
-              <p>16 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtRIEXv8_XPZVygG1ZQ"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos devicon-debian-plain colored" />
-              <h4>DevOps</h4>
-              <p>25 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtUoWMoxyIXb7u2vhlg"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos devicon-linux-plain colored" />
-              <h4>Linux</h4>
-              <p>32 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtVJHX4wfC6EQJme4YA"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos devicon-windows8-plain colored" />
-              <h4>Windows</h4>
-              <p>12 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtU0BZ1dY6ee0K3EWoQ"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos material-icons">blur_on</i>
-              <h4>Yapay Zeka ve Veri Bilimi</h4>
-              <p>53 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtU8wTomSdNKA2EO-0Q"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos material-icons">videogame_asset</i>
-              <h4>Oyun Geliştirme</h4>
-              <p>4 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtULsyqR4uGGfQMI8Qw"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos material-icons">format_shapes</i>
-              <h4>UX/UI Tasarım</h4>
-              <p>6 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtVg_ctqgJ96cmuvSwQ"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos material-icons">functions</i>
-              <h4>Matematik ve Algoritma</h4>
-              <p>26 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtUUj8nBxHk2OfnDTNg"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos material-icons">school</i>
-              <h4>İngilizce</h4>
-              <p>94 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtRGgWUViQ7fZzA93UQ"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos material-icons">work</i>
-              <h4>Kariyer</h4>
-              <p>62 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtUkgfGd9DJMfGUxGUw"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
-        <div className="col s12 m4 l3">
-          <div className="card hoverable">
-            <div className="card-content black-text">
-              <i className="logos material-icons">chat</i>
-              <h4>Geyik</h4>
-              <p>17 üye</p>
-            </div>
-            <a
-              href="https://t.me/joinchat/G0kqtUpxzWhIo5XMy7dpfg"
-              target="_blank"
-              className="btn-small waves-effect waves"
-            >
-              <img src={require("../assets/telegram.svg")} height="12em" />
-              Katıl
-            </a>
-          </div>
-        </div>
+        )
+      }) : "Yükleniyor..."}
       </div>
     </div>
-  );
-};
+    )
+  }
+}
 
 export default Telegram;
